9529c528241c6f3201db945b7808684f48d36e8d
Dmitriy <Dmiko@bk.ru>
2022-08-10 15:05:19 +0300

    Task3

---
+++ Seminar1_Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string? input = Console.ReadLine();
+int number = Int32.Parse(input);
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " является четным.");
+}
+else
+{
+    Console.WriteLine("Число " + number + " не является четным.");
+}
