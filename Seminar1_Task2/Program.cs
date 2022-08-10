0b7d4802f79129aff31cab615e5ce55fd7d26f01
Dmitriy <Dmiko@bk.ru>
2022-08-10 14:56:29 +0300

    Task2

---
+++ Seminar1_Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+string? input = Console.ReadLine();
+int numberA = Int32.Parse(input);
+Console.WriteLine("Введите второе число");
+input = Console.ReadLine();
+int numberB = Int32.Parse(input);
+Console.WriteLine("Введите третье число");
+input = Console.ReadLine();
+int numberC = Int32.Parse(input);
+int max = numberA;
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine("Максимальное из введенных чисел = " + max);
