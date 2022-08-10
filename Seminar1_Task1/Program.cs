52d75d7afbdbcd161ecd0fd470d3ab9b207fde68
Dmitriy <Dmiko@bk.ru>
2022-08-10 14:30:41 +0300

    Task1

---
+++ Seminar1_Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+string? input = Console.ReadLine();
+int numberA = Int32.Parse(input);
+Console.WriteLine("Введите второе число");
+input = Console.ReadLine();
+int numberB = Int32.Parse(input);
+if (numberA > numberB)
+{
+    Console.WriteLine("Число " + numberA + " больше, чем число " + numberB);
+}
+else if (numberA < numberB)
+{
+    Console.WriteLine("Число " + numberB + " больше, чем число " + numberA);
+}
+else Console.WriteLine("Оба числа равны.");
