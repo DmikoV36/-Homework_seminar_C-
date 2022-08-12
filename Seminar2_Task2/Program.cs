257f7db3bafb97d5643629980ce041ab91e79b67
Dmitriy <Dmiko@bk.ru>
2022-08-12 15:18:20 +0300

    Task2

---
+++ Seminar2_Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите положительное целое число");
+string? input = Console.ReadLine();
+int number = Int32.Parse(input);
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine("Третья цифра числа " + (number / 100) % 10);
+}
