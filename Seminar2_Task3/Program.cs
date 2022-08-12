bab0c0b92e512f15ac1db743db57087eadcfe6c7
Dmitriy <Dmiko@bk.ru>
2022-08-12 15:40:02 +0300

    Task3

---
+++ Seminar2_Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] array = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+Console.WriteLine("Введите цифру дня недели.");
+string? input = Console.ReadLine();
+int number = Int32.Parse(input);
+if (number > 7 || number < 1)
+{
+    Console.WriteLine("Такого дня недели не существует!");
+}
+else
+{
+    if (number > 5)
+    {
+        Console.WriteLine(array[number -1] + " является выходным днем."); 
+    }
+    else
+    {
+        Console.WriteLine(array[number -1] + " это не выходной.");
+    }
+}
