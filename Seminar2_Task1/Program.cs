052013ec85292d10e2f2ef1594d611f5c58df619
Dmitriy <Dmiko@bk.ru>
2022-08-12 14:56:34 +0300

    Task1

---
+++ Seminar2_Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите положительное трехзначное целое число");
+string? input = Console.ReadLine();
+int number = Int32.Parse(input);
+if (number >= 1000 || number < 100)
+{
+    while (number >= 1000 || number < 100)
+    {
+        Console.WriteLine("Что-то пошло не так)");
+        Console.WriteLine("Введите положительное трехзначное целое число");
+        input = Console.ReadLine();
+        number = Int32.Parse(input);
+    }
+}
+
+Console.WriteLine("Вторая цифра числа " + (number / 10) % 10);
