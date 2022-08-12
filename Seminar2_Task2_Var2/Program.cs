e57b63bd59ffcb04fde0647bc2b0e776bee7f843
Dmitriy <Dmiko@bk.ru>
2022-08-12 15:16:47 +0300

    Task2

---
+++ Seminar2_Task2_Var2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите положительное целое число");
+string? input = Console.ReadLine();
+
+if (input.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine("Третья цифра числа " + input[2]);
+}
