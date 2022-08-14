e55b8f9c30c86284a88409bb42527a0d733be883
Dmitriy <Dmiko@bk.ru>
2022-08-14 11:21:04 +0300

    Task3

---
+++ Seminar3_Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿void CubeValue (int n)
+{
+    for (int i = 1; i <= Math.Abs(n); i++)
+    {
+        Console.Write(Math.Pow(i, 3) + " ");
+    }
+}
+
+Console.WriteLine("Введите число");
+int n = Int32.Parse(Console.ReadLine());
+CubeValue(n);
