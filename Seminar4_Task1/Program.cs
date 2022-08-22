635c6a4d2a29d9d128f1f82b0069eade8aeaf3a3
Dmitriy <Dmiko@bk.ru>
2022-08-22 19:48:22 +0300

    Task1

---
+++ Seminar4_Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int exp = 1;
+int Exponentiation (int a, int b)
+{
+    for (int i = 1; i <= b; i++)
+    {
+        exp = exp * a;
+    }
+    return exp;
+}
+
+Console.WriteLine ("Введите число А");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine ("Введите число В");
+int b = Int32.Parse(Console.ReadLine());
+
+Exponentiation(a, b);
+Console.WriteLine ("Число А в степени В равно " + exp);
