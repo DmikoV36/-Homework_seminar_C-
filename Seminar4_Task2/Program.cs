a8fbe53c83827b7703729e6c5d7fe7e2739c53c8
Dmitriy <Dmiko@bk.ru>
2022-08-22 19:51:01 +0300

    Task2

---
+++ Seminar4_Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿void SumNumber (string input)
+{
+    int length = input.Length;
+    int number = Int32.Parse(input);
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    Console.WriteLine("Сумма цифр равна " + Math.Abs(sum));    
+
+}
+
+Console.WriteLine("Введите целое число");
+string? input = Console.ReadLine();
+
+SumNumber (input);
