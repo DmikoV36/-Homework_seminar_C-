cb315fde95c692a7753e2ac9e6ba9aec76cfa0e1
Dmitriy <Dmiko@bk.ru>
2022-08-22 19:53:19 +0300

    Task3

---
+++ Seminar4_Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine ("Введите количество элементов массива");
+int n = Int32.Parse(Console.ReadLine());
+int[] array = new int[n];
+
+void InputArray (int[] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < n - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine(array[n-1] + "]");
+}
+
+InputArray (array);
+PrintArray (array);
