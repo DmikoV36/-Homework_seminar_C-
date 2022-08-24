9c5d721649ca73bf157d6e50aafa4d5d43275f94
Dmitriy <Dmiko@bk.ru>
2022-08-24 20:16:09 +0300

    Task2

---
+++ Seminar5_Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine ("Введите количество элементов массива");
+int n = Int32.Parse(Console.ReadLine());
+int[] array = new int[n];
+
+void InputArray (int[] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+    Console.WriteLine(string.Join(", ", array));
+}
+
+void SumOddIndex (int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < n; i += 2)
+    {
+        sum = sum + array[i];
+    }
+    Console.WriteLine("Сумма элементов, стоящих на нечётных позициях = " + sum);
+}
+
+InputArray (array);
+SumOddIndex (array);
