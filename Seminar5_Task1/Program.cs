ee7b2191735965daf8128cdd5690fad14f1610c1
Dmitriy <Dmiko@bk.ru>
2022-08-24 20:14:34 +0300

    Task1

---
+++ Seminar5_Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine ("Введите количество элементов массива");
+int n = Int32.Parse(Console.ReadLine());
+int[] array = new int[n];
+
+void InputArray (int[] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    Console.WriteLine(string.Join(", ", array));
+}
+
+void NumberOfEven (int n)
+{
+    int number = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            number += 1;
+        }
+    }
+    Console.WriteLine ("Количество четных элементов массива = " + number);
+}
+
+
+InputArray (array);
+NumberOfEven (n);
